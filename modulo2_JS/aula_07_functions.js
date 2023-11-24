b3a5557a5385da635439601868e796b28d433e93
Michely Prado <michely_prado@hotmail.com>
2023-11-24 18:01:51 -0300

    FEAT: add Try/Catch / Jogo da velha

--- modulo2_JS/aula_07_functions.js
+++ modulo2_JS/aula_07_functions.js
@@ -131,67 +131,77 @@ while (stop == false) {
 // 03- Jogo da Velha: implemente o clássico jogo da velha em JavaScript. Permita que dois jogadores se alternem para fazer suas jogadas e determine o vencedor.
 
 function mostraTabuleiro(valor) {
-    for (let i = 0; i < 3; i++) {
-        console.log(valor[i].join(' | '));
-        if (i < 2) console.log('---------');
-      if (i == 2){
-        console.log('Nova jogada')
-      }
-    }
+	let valorStr = '';
+	for (let i = 0; i < 3; i++) {
+		for (let j = 0; j < 3; j++) {
+			valorStr += valor[i][j] + '   ';
+		}
+		valorStr += '\n';
+	}
+	alert(valorStr);
 }
 
 function verificaVencedor(valor, jogador) {
-    for (let i = 0; i < 3; i++) {
-        if (
-            (valor[i][0] === jogador && valor[i][1] === jogador && valor[i][2] === jogador) || // linha
-            (valor[0][i] === jogador && valor[1][i] === jogador && valor[2][i] === jogador) // coluna
-        ) {
-            return true;
-        }
-    }
-    if (
-        (valor[0][0] === jogador && valor[1][1] === jogador && valor[2][2] === jogador) ||
-        (valor[0][2] === jogador && valor[1][1] === jogador && valor[2][0] === jogador)
-    ) {
-        return true;
-    }
-    return false;
+	for (let i = 0; i < 3; i++) {
+		if (
+			(valor[i][0] === jogador && valor[i][1] === jogador && valor[i][2] === jogador) || // linha
+			(valor[0][i] === jogador && valor[1][i] === jogador && valor[2][i] === jogador) // coluna
+		) {
+			return true;
+		}
+	}
+
+	if (
+		(valor[0][0] === jogador && valor[1][1] === jogador && valor[2][2] === jogador) ||
+		(valor[0][2] === jogador && valor[1][1] === jogador && valor[2][0] === jogador)
+	) {
+		return true;
+	}
+
+	return false;
 }
 
 function jogar() {
+	const valor = [
+		['1', '2', '3'],
+		['4', '5', '6'],
+		['7', '8', '9'],
+	];
 
-    const valor = [
-        [' ', ' ', ' '],
-        [' ', ' ', ' '],
-        [' ', ' ', ' ']
-    ];
+	let jogadorAtual = 'X';
 
-    let jogadorAtual = 'X';
-    let fimJogo = false;
+	while (true) {
+		mostraTabuleiro(valor);
 
-    while (!fimJogo) {
-        mostraTabuleiro(valor);
+		const escolha = prompt(`Jogador ${jogadorAtual}, informe um número de 1 a 9: `);
 
-        const linha = prompt(`Jogador ${jogadorAtual}, informe a linha (0, 1 ou 2):`);
-        const coluna = prompt(`Jogador ${jogadorAtual}, informe a coluna (0, 1 ou 2):`);
+		if (isNaN(escolha) || escolha < 1 || escolha > 9) {
+			alert('Escolha inválida. Tente novamente.');
+			continue;
+		}
 
-        if (valor[linha][coluna] === ' ') {
-            valor[linha][coluna] = jogadorAtual;
+		let linha = Math.floor((escolha - 1) / 3);
+		let coluna = (escolha - 1) % 3;
 
-            if (verificaVencedor(valor, jogadorAtual)) {
-                mostraTabuleiro(valor);
-                alert(`Parabéns! O jogador ${jogadorAtual} venceu.`);
-                fimJogo = true;
-            } else {
-                jogadorAtual = jogadorAtual === 'X' ? 'O' : 'X';
-            }
-        } else {
-            alert('Posição ocupada! Tente novamente.');
-        }
-    }
+		if (valor[linha][coluna] === 'X' || valor[linha][coluna] === 'O') {
+			alert('Posição ocupada. Tente novamente.');
+			continue;
+		}
+
+		valor[linha][coluna] = jogadorAtual;
+
+		if (verificaVencedor(valor, jogadorAtual)) {
+			mostraTabuleiro(valor);
+			alert(`Parabéns! O jogador ${jogadorAtual} venceu.`);
+			break;
+		}
+		jogadorAtual = jogadorAtual === 'X' ? 'O' : 'X';
+	}
 }
 
 jogar();
+
+        
 
 
 //alert inves de mostrar tab no console log, jogar no alert
